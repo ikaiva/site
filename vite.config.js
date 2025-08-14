54eb0e32eab0e6bec6d48b970bef5140f10097ac
SurendharGN <work.surendhar@gmail.com>
2025-08-15 00:13:18 +0530

    changed dependencies

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  base: 'https://github.com/ikaiva/site', // Replace <YOUR-REPO-NAME> with your repository name
+  base: '/site', // Replace <YOUR-REPO-NAME> with your repository name
 })
