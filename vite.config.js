b1fb6f6342008b525796d96d87fa19ddba58a5f5
SurendharGN <work.surendhar@gmail.com>
2025-08-14 13:39:03 +0530

    hosting in github

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  plugins: [react(), tailwindcss(),],
+  base: 'https://github.com/ikaiva/site', // Replace <YOUR-REPO-NAME> with your repository name
 })
